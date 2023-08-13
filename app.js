596f935ef6a7bc68c29e563d9e83be2d47766d90
SIMON WACHIRA <simonwachira7318@gmail.com>
2023-08-13 17:11:18 +0300

    updated web

--- app.js
+++ app.js
@@ -6,3 +6,30 @@ var button = document.querySelector("button");
 button.addEventListener("click", function() {
   header.classList.toggle("active");
 });
+
+
+
+
+
+
+
+
+
+
+// Get the button
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+// When the user clicks on the button, scroll to the top of the document
+scrollToTopBtn.addEventListener("click", function () {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, Opera
+});
