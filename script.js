0039ac4f95f2ec010b6c5a960c826f9cdf92494d
SIMON WACHIRA <simonwachira7318@gmail.com>
2023-08-16 12:27:36 +0300

    updated web

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ document.getElementById("back-button").addEventListener("click", function() {
 // Get the button
 var scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down from the top of the document, show the button
 window.onscroll = function () {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     scrollToTopBtn.style.display = "block";
@@ -36,7 +36,7 @@ window.onscroll = function () {
   }
 };
 
-// When the user clicks on the button, scroll to the top of the document
+// When clicks on the button, scroll to the top of the document
 scrollToTopBtn.addEventListener("click", function () {
   document.body.scrollTop = 0; 
   document.documentElement.scrollTop = 0;
