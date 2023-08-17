e196d3cd8b4140680a38de15f3df443494cbc43f
SIMON WACHIRA <simonwachira7318@gmail.com>
2023-08-17 11:06:22 +0300

    updated web

---
+++ send_email.php
@@ -0,0 +1,27 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $service = $_POST['service'];
+    $message = $_POST['message'];
+
+    $to = 'simonwachira7318@gmail.com';  // send email to this email address
+    $subject = 'New Contact Form Submission';
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n";
+    $email_content .= "Service: $service\n";
+    $email_content .= "Message:\n$message";
+
+    if (mail($to, $subject, $email_content, $headers)) {
+        echo "Thank you! Your message has been sent.";
+    } else {
+        echo "Oops! Something went wrong and we couldn't send your message.";
+    }
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    
+}
+?>
